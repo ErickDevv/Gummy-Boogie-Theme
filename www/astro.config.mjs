b93945f55ef82811949b42511ea1e4dbc4061e4a
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2024-01-23 13:00:31 -0600

    Migration to Starlight

---
+++ www/astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false
+  }), starlight({
+    title: 'My Docs',
+    customCss: [
+      // Path to your Tailwind base styles:
+      './src/tailwind.css'],
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+        // Each item here is one entry in the navigation menu.
+        {
+          label: 'Example Guide',
+          link: '/guides/example/'
+        }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), tailwind(), react()]
+});
