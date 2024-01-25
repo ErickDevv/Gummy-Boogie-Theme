c1b6fd25b2caa29f5af592dea4c6aa9ce576a06d
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2024-01-25 08:27:59 -0600

    Add Integrations

--- www/tailwind.config.mjs
+++ www/tailwind.config.mjs
@@ -1,7 +1,8 @@
-import starlightPlugin from '@astrojs/starlight-tailwind';
+import starlightPlugin from "@astrojs/starlight-tailwind";
+import icon from "astro-icon";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  plugins: [starlightPlugin()],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [starlightPlugin(), icon()],
 };
